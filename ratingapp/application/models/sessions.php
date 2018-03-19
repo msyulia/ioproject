284737f0d4d241714bc7d4f1879c1b8177b4efdd
Michał <nt.michal@gmail.com>
2018-03-19 17:37:50 +0100

    Sesje
    doodane get

--- ratingapp/application/models/sessions.php
+++ ratingapp/application/models/sessions.php
@@ -1,8 +1,50 @@
 <?php
-    //sesje, potem ktoś będzie pisał logowanie jako późniejszy Task
-    //maja być przygotowanie pola pod PESEL , Imie, Nazwisko
-    class Sessions{
+
+    class Sessions
+    {
+        private static $_sessionStart = false;
+
+        public static function startSession(){
+            if(self::$_sessionStart == false){
+                session_start();
+                self::$_sessionStart = true;
+            }
+        }
+        public static function stopSession(){
+            if(self::$_sessionStart == true){
+                session_destroy();
+                self::$_sessionStart = false;
+            }
+        }
+        public static function setImie($imie){
+            $_SESSION['imie'] = $imie;
+        }
+        public static function setNazwisko($nazwisko){
+            $_SESSION['nazwisko'] = $nazwisko;
+        }
+
+        public static function setLogin($login){
+            $_SESSION['login'] = $login;
+        }
+
+        public static function getImie(){
+            return $_SESSION['imie'];
+        }
+        public static function getNazwisko(){
+            return $_SESSION['nazwisko'];
+        }
+
+        public static function getLogin(){
+            return $_SESSION['login'];
+        }
+
+        public static function isLogged(){
+            if(isset($_SESSION['imie']) && isset($_SESSION['nazwisko']) && isset($_SESSION['login'])){
+                return "tak";
+            }else{
+                echo "nie";
+            }
+        }
 
     }
-
 ?>
