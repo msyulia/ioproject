6995f06ae09ca3be49607eb0f272c3e2727dd9b7
Michał <nt.michal@gmail.com>
2018-03-22 11:24:29 +0100

    LoginWorksV2

--- ratingapp/application/controllers/login.php
+++ ratingapp/application/controllers/login.php
@@ -27,7 +27,6 @@ else
         }
         else
         {
-<<<<<<< HEAD
             // $dbLogin = dbConnection::sendquery('select login from pracownicy where login='.$Login);
             $dbLogin = dbConnection::sendquery("SELECT * FROM pracownicy WHERE login='$Login'");
             $resultCheck = mysqli_num_rows($dbLogin);
@@ -48,43 +47,15 @@ else
                     // Sessions::setImie($Imie);
                     // Sessions::setNazwisko($Nazwisko);
                     redirect("../../index.php?success");    //Zmienić na odpowiednią stronę   
-=======
-            $dbLogin = dbConnection::sendquery('select login from pracownicy where login='.$login);
-            if(empty($dbLogin['login']))
-            {
-                //Użytkownika nie ma w bazie
-                errorRedirect('?form=userexists');
-            }
-            else
-            {
-                //Formularz dobrze wypełniony użytkownik chce się zalogować    
-                //Sprawdź poprawność danych później jakaś walidacja loginu i hasła
-                $pwd = password_hash($pwd, PASSWORD_DEFAULT);
-                $queryres = dbConnection::sendquery('select password from pracownicy where login='.$Login);
-                if($Pwd==$queryres['password'])
-                {
-                    //Poprawne hasło
-                    $Imie = dbConnection::sendquery('select Imie from pracownicy where login='.$Login);
-                    $Nazwisko = dbConnection::sendquery('select Nazwisko from pracownicy where login='.$Login);
-                    Sessions::setLogin($Login);
-                    Sessions::setImie($Imie);
-                    Sessions::setNazwisko($Nazwisko);
-
-                    redirect("../../index.php");    //Zmienić na odpowiednią stronę   
->>>>>>> 25bf217b3f071ec711088162e8489a2c356d8900
                 }
                 else
                 {
                     //Hasło niepoprawne 
                     errorRedirect('?form=incorrect');
-<<<<<<< HEAD
                 }                   
                 }
             }else{
                 errorRedirect('?form=userexists');
-=======
-                }
->>>>>>> 25bf217b3f071ec711088162e8489a2c356d8900
             }
 
         }
