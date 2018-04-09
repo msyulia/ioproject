ef3f1abf04f3277f68b45a2a75aa0b9af16b384c
Michał <nt.michal@gmail.com>
2018-04-09 22:27:01 +0200

    nowe klasy
    -searchEngine.php
    -registerModel.php
    -loginModel.php

--- ratingapp/application/application.php
+++ ratingapp/application/application.php
@@ -1,13 +1,20 @@
 <?php
+
+
+
+
+
     //MODELS
     require 'models/dbConnection.php';
     require 'models/employerComments.php';
     require 'models/sessions.php';
     require 'models/accessAuthorization.php';
     require 'models/reviewsAndComments.php';
+    require 'models/searchEngine.php';
+    require 'models/registerModel.php';
+    require 'models/loginModel.php';
 
     //CONTROLLERS
     require 'controllers/comments.php';
-
     
 ?>
