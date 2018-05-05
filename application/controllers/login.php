964efbb601126da02a41cbd15a1e3d6b6fd09b38
Michał <nt.michal@gmail.com>
2018-05-05 12:26:51 +0200

    Dostęp z zewnątrz
    Blokada dostępu z zwenątrz

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,16 +1,20 @@
 <?php
 require "../application.php";
 
-$fLogin = $_POST['login'];
-$fPwd =  $_POST['password'];
-
-if(empty($fLogin) || empty($fPwd))
-{
-    echo 'Uzupełnij wszystkie pola';
-}
-else
-{
-    $obj = new Login($fLogin,$fPwd);
-    $obj->login();
+if(isset($_POST['submit'])){
+    $fLogin = $_POST['login'];
+    $fPwd =  $_POST['password'];
+    
+    if(empty($fLogin) || empty($fPwd))
+    {
+        echo 'Uzupełnij wszystkie pola';
+    }
+    else
+    {
+        $obj = new Login($fLogin,$fPwd);
+        $obj->login();
+    }
+}else{
+    header("Location: ../../views/login.php");
 }
 ?>
